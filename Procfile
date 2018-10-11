265410335c3f19695d34d881720d2ebb44945d0e
William Ahrons <william.ahrons@gmail.com>
2018-10-11 20:30:07 -0300

    Add profiling on Filter

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/wine-0.0.1-SNAPSHOT.jar --spring.profiles.active=heroku --server.port=$PORT
--- src/main/java/io/wine/config/FilterConfiguration.java
+++ src/main/java/io/wine/config/FilterConfiguration.java
@@ -1,11 +1,11 @@
 package io.wine.config;
 
-import io.wine.exception.SessionExpiredException;
 import io.wine.model.User;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
+@Profile("heroku")
 public class FilterConfiguration {
 
     @Bean
